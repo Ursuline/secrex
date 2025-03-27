d0bddf7c37d18f79b660fd1f00616982497f11ae
Ursuline <3534155+Ursuline@users.noreply.github.com>
2025-03-27 10:21:31 +0100

    initial commmit

---
+++ driver.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: charles mégnin
+
+The Driver class runs a single call for a ticker
+"""
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+import downloader
+import frame
+import config
+import request
+import objective_function
+import objective_function_plotter as of_plotter
+import time_series_plotter as ts_plotter
+
+class Driver():
+    def __init__(self, conf:config.Config, ticker:str, end_date:str, nmonths:int):
+        self._conf = conf
+        self._ticker = ticker
+        self._end_date = end_date
+        self._nmonths = nmonths
+
+    def drive(self):
+        # Build the request object
+        self._req = request.Request(
+            conf=self._conf,
+            ticker=self._ticker,
+            start=datetime.strptime(self._end_date, self._conf.get_date_format())
+            - relativedelta(months=self._nmonths),
+            end=datetime.strptime(self._end_date, self._conf.get_date_format()),
+        )
+
+        self._download = downloader.Downloader(self._conf, self._req)
+        raw, _ = self._download.get_time_series()
+        ts_frame = frame.Frame(self._conf, raw, self._req)
+        self._of = objective_function.ObjectiveFunction(
+            conf=self._conf,
+            frm=ts_frame,
+            req=self._req,
+        )
+
+        if self._conf.get_config_parameters()["save_data"]:
+            # Write frame time series to csv file
+            ts_frame.to_csv(self._conf.get_config_parameters()["data_dir"])
+            # Write objective function to csv file
+            self._of.save_data(self._conf.get_config_parameters()["data_dir"])
+
+        with pd.option_context(
+            "display.max_rows",
+            self._conf.get_pandas_display()["max_rows"],
+            "display.max_columns",
+            self._conf.get_pandas_display()["max_columns"],
+            "display.width",
+            self._conf.get_pandas_display()["width"],
+            "display.precision",
+            self._conf.get_pandas_display()["precision"],
+            "display.colheader_justify",
+            self._conf.get_pandas_display()["colheader_justify"],
+        ):
+            #if conf.get_debug():
+                #print('Objective function maxima:')
+                #io_util.pretty_print(of.get_local_maxima())
+
+            date_range = self._req.get_dates('actual')
+            if self._conf.get_debug():
+                print(f"\nSecurity: {self._ticker}")
+                print(f'Date range: {date_range["start_date"].date()} -> {date_range["end_date"].date()}')
+
+        plot_instance = of_plotter.ObjectiveFunctionPlotter(
+            conf=self._conf, req=self._req, of=self._of
+        )
+        plot_instance.plot()
+
+        #conf.print_parameters()
+
+        plot_instance = ts_plotter.TimeSeriesPlotter(
+            conf=self._conf, req=self._req, of=self._of, frm=ts_frame
+        )
+        plot_instance.plot()
